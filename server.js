c00d3e1e74e66f0b9500fc39463e93a4965ae246
makakoa <makakoa15@gmail.com>
2014-11-05 09:56:28 -0800

    added grunt test jscs and jshint

--- server.js
+++ server.js
@@ -5,30 +5,30 @@ var fs = require('fs');
 var app = express();
 app.use(bp.json());
 
-app.route('/:some_name')
+app.route('/:someName')
 
-    .get(function( req, res) {
-        var file = ('./db/' + req.params.some_name + '.json');
-        var stream = fs.createReadStream(file);
-        stream.on('error', function(err) {
-            console.log('Error', err);
-            res.send(err);
+    .get(function(req, res) {
+      var file = ('./db/' + req.params.someName + '.json');
+      var stream = fs.createReadStream(file);
+      stream.on('error', function(err) {
+          console.log('Error', err);
+          res.send(err);
         });
-        stream.on('readable', function() {
-            res.writeHead(200, {"Content-Type": "application/json"});
-            stream.pipe(res);
-        });
+      stream.on('readable', function() {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        stream.pipe(res);
+      });
     })
 
-    .post(function( req, res) {
-        res.writeHead(200);
-        var ws = fs.createWriteStream('./db/' + req.params.some_name + '.json');
-        ws.write(JSON.stringify(req.body));
-        ws.end();
-        res.end();
+    .post(function(req, res) {
+      res.writeHead(200);
+      var ws = fs.createWriteStream('./db/' + req.params.someName + '.json');
+      ws.write(JSON.stringify(req.body));
+      ws.end();
+      res.end();
     });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-    console.log("Server up");
+  console.log('Server up');
 });
